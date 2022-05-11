6099f764c416d9b7cfb30a5eaa19a4587cceafd3
anis rbh <mohamedrbh519@gmail.com>
2022-05-11 10:23:08 +0100

    1- ajout header (part 1)

---
+++ components/Header/headerStyles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+export default styles = StyleSheet.create({
+    header: {
+        backgroundColor: '#093357',
+        paddingHorizontal: 40,
+        paddingVertical: 20,
+        borderRadius: 5,
+    },
+    title: {
+        textAlign: 'center',
+        color: '#bbb',
+        fontSize: 32,
+        fontWeight: 'bold',
+    },
+    text: {
+        fontSize: 20,
+        color: '#bbb',
+        textAlign: 'center'
+    }
+})
