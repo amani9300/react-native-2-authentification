f4b1b2b4e65d651094fe4f59f63a4dbaaebe8699
anis rbh <mohamedrbh519@gmail.com>
2022-05-11 15:54:33 +0100

    11 - ajout text erreur ( part 1 )

--- components/Ui/Input/Input.jsx
+++ components/Ui/Input/Input.jsx
@@ -2,14 +2,17 @@ import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
 import styles from './inputStyles';
 // props => {children: ....., }
-const Input = ({ handlerFunction,val,children }) => {
+const Input = ({ handlerFunction, val, children }) => {
     return (
-        <View style={styles.container}>
-            {/* l'icone */}
-            <View>{children}</View>
-            {/* l'input */}
-            <TextInput onChangeText={handlerFunction} value={val} style={styles.input} />
-        </View>
+        <>
+            <View style={styles.container}>
+                {/* l'icone */}
+                <View>{children}</View>
+                {/* l'input */}
+                <TextInput onChangeText={handlerFunction} value={val} style={styles.input} />
+            </View>
+            <Text></Text>
+        </>
     )
 }
 
