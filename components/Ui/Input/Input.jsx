50df5ea20c45b230b6e1a85b31c25b2008e997e1
anis rbh <mohamedrbh519@gmail.com>
2022-05-11 11:13:29 +0100

    3 - ajout input (part 1 )

---
+++ components/Ui/Input/Input.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React from 'react'
+
+const Input = () => {
+  return (
+    <View>
+        {/* l'icone */}
+      <View></View>
+      {/* l'input */}
+      <TextInput />
+    </View>
+  )
+}
+
+export default Input
+
---
+++ components/Ui/Input/inputStyles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+export default StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '90%',
+        borderWidth: 1,
+        borderColor: 'gray',
+        marginVertical: 15,
+        paddingHorizontal: 10
+
+    },
+    input: {
+        padding: 10,
+        width: '90%'
+    },
+ 
+})
+
